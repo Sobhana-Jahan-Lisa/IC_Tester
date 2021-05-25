3a94d46d121456773cc981fb8e797a46c6c68676
Sobhana-Jahan-Lisa <74866450+Sobhana-Jahan-Lisa@users.noreply.github.com>
2021-05-25 15:51:09 +0600

    Add files via upload

---
+++ IC_TESTER.ino
@@ -0,0 +1,24 @@
+int gate1A=11;
+int gate1B=12;
+int gate1o=13;
+
+void setup() {
+pinMode(gate1A,OUTPUT);
+pinMode(gate1B,OUTPUT);
+pinMode(gate1o,INPUT);
+
+}
+
+void loop() {
+ 
+digitalWrite(gate1A,HIGH);
+digitalWrite(gate1A,LOW);
+if(digitalRead(gate1o)==HIGH)
+  {
+  serial.print("OR GATE");
+  }
+else
+  {
+    serial.print("AND GATE"); 
+  }
+}
